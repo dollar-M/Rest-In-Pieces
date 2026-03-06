bbd1fe7cb78e479abb6337910eb8aaeaca484cae
zero <62719887+zerori3jir@users.noreply.github.com>
2026-03-05 22:54:34 -0500

    Added simple movement, gravity, jumping
    yeah

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -1,52 +1,179 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class NewMonoBehaviourScript : MonoBehaviour
-// {
-//     public float speed = 5f; // Speed of the player movement
-//     private Vector2 movement; // Variable to store movement input
-//     private Animator animator; 
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-//          animator = GetComponent<Animator>();
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         float input =Input.GetAxisRaw("Horizontal"); // Get horizontal input on keys
-//         movement.x = input * Time.deltaTime * speed; // only move horizontally, multiply by deltaTime for frame rate independence
-//         transform.Translate(movement); // Move the player based on the movement vector
-//         if (input != 0) //no input from key
-//         {
-//             animator.SetBool("isRunning", true); 
-//         }
-//         else
-//         {
-//             animator.SetBool("isRunning", false); 
-//         }
-//     }
-// }
-
+using System;
 using UnityEngine;
 
-public class CharacterAnimator : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private Animator animator;
-    private Rigidbody2D parentRb;
+    /*
+     * boxcollider2d + isPhaseable on the objectproperties script
+     */
 
-    void Awake()
+    public enum PlayerStage
     {
-        animator = GetComponent<Animator>();
-        parentRb = transform.parent.GetComponent<Rigidbody2D>();
+        Ghost,
+        Leg,
+        Arm,
+        Torso,
+        Head
+    }
+
+    private Rigidbody2D rb;
+    private Collider2D playerCollider;
+    private float moveInput;
+
+    // Our commonly used variables
+
+    [Header("Player form")]
+    public PlayerStage currentStage = PlayerStage.Ghost;
+
+    // We can start with 5, we can always change it
+    public float moveSpeed = 5f;
+
+
+    [Header("Phasing")]
+    public KeyCode phaseKeyBind = KeyCode.E;
+    public float phaseDuration = 4f;
+    public bool playerCanPhase = false;
+
+    private bool isPhasing = false;
+    private float currentPhasingTimer;
+
+    [Header("Jumping")]
+    public KeyCode jumpKeyBind = KeyCode.Space;
+    public float jumpForce = 8f;
+    public bool playerCanJump = false;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool onGround = true;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        ChangePlayerStage(currentStage);
     }
 
     void Update()
     {
-        // Use parent's velocity to set animation
-        bool isRunning = parentRb.linearVelocity.magnitude > 0.1f;
-        animator.SetBool("isRunning", isRunning);
+        moveInput = Input.GetAxisRaw("Horizontal");
+        // Check if the player is on the ground
+        if (groundCheck != null)
+        {
+            onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        }
+
+        // Check if the player is a ghost, check if they click the phasing keybind
+        if (GetInput(phaseKeyBind) && currentStage == PlayerStage.Ghost && !isPhasing)
+        {
+            StartPhasing();
+        }
+
+        Debug.Log(onGround.ToString());
+        if (GetInput(jumpKeyBind) && currentStage == PlayerStage.Leg && playerCanJump && onGround)
+        {
+            Debug.Log("JUMP");
+            Jump();
+        }
+
+        // If player is currently phasing, count the timer down
+        if (isPhasing)
+        {
+            currentPhasingTimer -= Time.deltaTime;
+
+            if (currentPhasingTimer <= 0f)
+            {
+                StopPhasing();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        // Do all the stuff to make the player jump
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    // This just changes the player stages
+    public void ChangePlayerStage(PlayerStage stage)
+    {
+        currentStage = stage;
+
+        // Reset what the player can do
+        playerCanPhase = false;
+        playerCanJump = false;
+
+        // If we're not ghost anymore, stop phasing
+        if (currentStage != PlayerStage.Ghost)
+        {
+            StopPhasing();
+        }
+
+        // Set what this stage is allowed to do
+        switch (currentStage)
+        {
+            case PlayerStage.Ghost:
+                playerCanPhase = true;
+                rb.gravityScale = 3f;
+                break;
+
+            case PlayerStage.Leg:
+                playerCanJump = true;
+                rb.gravityScale = 3f;
+                break;
+        }
+    }
+
+    void StartPhasing()
+    {
+        // Do all the stuff to start phasing
+        isPhasing = true;
+        currentPhasingTimer = phaseDuration;
+        playerCanPhase = true;
+
+        SetPhaseCollision(true);
+    }
+
+    void StopPhasing()
+    {
+        // Do all the stuff to stop phasing
+        isPhasing = false;
+        currentPhasingTimer = 0f;
+        playerCanPhase = false;
+
+        SetPhaseCollision(false);
+    }
+
+    void SetPhaseCollision(bool ignoreCollision)
+    {
+        // Go through every phaseable object in the list
+        foreach (ObjectProperties obj in ObjectProperties.phaseableObjects)
+        {
+            if (obj == null)
+                continue;
+
+            Collider2D objectCollider = obj.GetComponent<Collider2D>();
+
+            if (objectCollider != null && playerCollider != null)
+            {
+                Physics2D.IgnoreCollision(playerCollider, objectCollider, ignoreCollision);
+            }
+        }
+    }
+
+    // This is just to save writing some shit
+    private bool GetInput(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+            return true;
+        return false;
     }
 }
